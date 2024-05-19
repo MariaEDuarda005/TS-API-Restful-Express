e68cca2bcf8adb67b7c14c1cc74bbc9ad8feb2ed
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-05-19 19:04:49 -0300

    Reconectando com o Banco de dados

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,19 @@
+// Importe o módulo dotenv
+require('dotenv').config();
+
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
+
+
+// Use as variáveis de ambiente conforme necessário
+console.log("Usuário do banco de dados:", dbUser);
+console.log("Senha do banco de dados:", dbPassword);
 
 export default {
     port: 3000,
     dbUri: 
-        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.mchhbkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&appName=Cluster0`,
+        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.cfac1rn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&appName=Cluster0`,
     env: "development",
 };
+
+// mongodb+srv://mf548800:<password>@cluster0.cfac1rn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 - mchhbkj
